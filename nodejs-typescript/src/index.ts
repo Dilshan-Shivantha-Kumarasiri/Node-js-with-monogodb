50a49e482d6ef11a82566593121694976b8d626f
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-05 11:00:42 +0530

    assign express to the port and tell express to listen that port

--- nodejs-typescript/src/index.ts
+++ nodejs-typescript/src/index.ts
@@ -2,3 +2,20 @@ import express from "express";
 
 //invoking the express
 const app =express();
+
+
+
+
+
+
+
+
+
+
+
+
+// assign server port and tell the express to listen that poprt
+
+app.listen(8080 , ()=>{
+    console.log("server is started");
+})
