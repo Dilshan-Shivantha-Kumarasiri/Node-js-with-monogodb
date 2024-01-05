bfa9388675d2a3dcb04da329809e0c1073253f40
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-05 15:11:26 +0530

    response json object

--- nodejs-typescript/src/index.ts
+++ nodejs-typescript/src/index.ts
@@ -8,8 +8,17 @@ const app =express();
 //Because routes are coming with the express
 app.get('/user/all', (req:express.Request , res:express.Response) => {
 
+    let data ={
+        _id:"M-001",
+        fName:"Dilshan",
+        lName:"Shivantha",
+        email:"dilshan@3002.com"
+    }
+
+
     //the response that we need to send back when request is coming
-    res.send("Hello");
+    //res.send("Hello");
+    res.send(data);
 });
 
 
