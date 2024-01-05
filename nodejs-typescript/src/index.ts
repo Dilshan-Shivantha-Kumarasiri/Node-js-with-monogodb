f502c03dcd5727d87f9304266bffbd9e0e07e4bc
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-05 15:38:40 +0530

    apply body passer and send response with the status code

--- nodejs-typescript/src/index.ts
+++ nodejs-typescript/src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
-
+import bodyParser from "body-parser";
 //invoking the express
 const app =express();
 
+//applying middleware
+//invoking bodyparser
+app.use(bodyParser.json())
 
 //in node js this is not the endpoint this called as the routes
 //Because routes are coming with the express
@@ -22,13 +25,20 @@ app.get('/user/all', (req:express.Request , res:express.Response) => {
 });
 
 
+//post method
+/*
+*
+*Crate new user
+*/
+app.post('/user' ,(req:express.Request , res: express.Response) =>{
 
+    //if want to install out 3rd party library because after 2020 we can not use this directly
+    //Becaue it need to convert to the format for that we use bodyparser lib
+    console.log(req.body);
 
-
-
-
-
-
+    //set the response with the status code
+    res.status(500).send(req.body)
+})
 
 
 // assign server port and tell the express to listen that poprt
