b913b91eb5ab04dbb2c01bf9822f442a44505f7f
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-08 04:54:58 +0530

    update article

--- nodejs-typescript/src/index.ts
+++ nodejs-typescript/src/index.ts
@@ -10,7 +10,7 @@ import CustomeResponse from "./dtos/custome.response";
 import ArticleModel from "./models/article.model";
 import {ObjectId} from "mongodb";
 import * as process from "process";
-import jwt, {Secret} from 'jsonwebtoken'
+import jwt, {Jwt, Secret, verify} from 'jsonwebtoken'
 
 //invoking the express
 const app = express();
@@ -137,18 +137,38 @@ app.post("/user/auth", async (req: express.Request, res: express.Response) => {
     decoding and verify the JWT token through middleware functions
     custom middlewear
 */
-const verifyToken = (req:express.Request,res:any,next:express.NextFunction) => {
+const verifyToken = (req:express.Request , res:any, next:express.NextFunction) => {
+    //get the access token from the header
+    const token = req.headers.authorization;
+
+    if (!token){
+        //check the token is available
+        return res.status(401).json('authorized denied')
+    }
+
+    try {
+        const data = jwt.verify(token, process.env.SECRET as Secret);
+        // console.log(data);
+        res.tokenData = data;
+        next();
+    }catch (e) {
+        return res.staus(401).json('authorized denied')
+    }
+
 
 }
 
-app.post("/article", verifyToken,async (req: express.Request, res: express.Response) => {
+app.post("/article", verifyToken,async (req: express.Request, res: any|express.Response) => {
     try {
+
+        //console.log(res);
+        const user_id = res.tokenData.user._id;
 
         let req_body = req.body;
         const articleModel = new ArticleModel({
             title: req_body.title,
             description: req_body.description,
-            user: new ObjectId(req_body.user)
+            user: new ObjectId(user_id)
         })
 
         await articleModel.save().then(r => {
@@ -187,6 +207,69 @@ app.get("/article", async (req: express.Request, res: express.Response) => {
     }
 })
 
+app.get("/articles/my", verifyToken, async(req, res:any) => {
+    try {
+
+        //console.log(req.params)
+        const username: any = req.params.userName;
+        let req_query: any = req.query;
+        let size: number = req_query.size;
+        let page: number = req_query.page;
+
+        let user_id = res.tokenData.user._id;
+
+        //find the user avaialbel with the user name
+        let users = await UserModel.findOne({_id:user_id});
+        if (!users) {
+            res.send(new CustomeResponse(404,"user not found"))//new CustomeResponse(404,"user not found");
+        }else {
+            // @ts-ignore
+            let articles = await ArticleModel.find({user: users._id}).limit(size).skip(size * (page-1));
+            let documentCount = await ArticleModel.countDocuments({user: users._id})
+            let pageCount = Math.ceil(documentCount/size);
+
+            res.status(200).send(new CustomeResponse(200,
+                'articles found success',articles,pageCount));
+        }
+
+        res.send("ok")
+
+
+    } catch (error) {
+
+    }
+})
+
+app.put("articles",verifyToken ,async (req, res:any) =>{
+
+    const article_id = req.body.id;
+
+    try{
+        let user_id = res.tokenData.user._id;
+        const article = await ArticleModel.find({_id:article_id , user:user_id})
+
+        if(article){
+
+            await ArticleModel.findOneAndUpdate({_id:article_id},{title:req.body.titile , description:req.body.description}).
+            then(r => {
+                res.status(200).send(new CustomeResponse(200,"article update success"))
+            }).
+            catch(e => {
+                res.status(100).send(new CustomeResponse(100,"something went wrong"))
+            });
+
+        }else {
+            res.status(401).send(new CustomeResponse(401,"can not access"))
+        }
+
+    }catch (error) {
+        res.status(500).json("error");
+    }
+
+
+});
+
+/*
 app.get("/articles/:userName", async(req, res) => {
     try {
 
@@ -219,7 +302,7 @@ app.get("/articles/:userName", async(req, res) => {
 
     }
 })
-
+*/
 
 // create connection with mongodb using mongoose
 mongoose.connect(process.env.MONGO_URL as string);
