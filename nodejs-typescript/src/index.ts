2e89d40c1092dfbbfbf390ee06849dd99da3235e
dilshan-shivantha <shiwanthadilshan3@gmail.com>
2024-01-07 17:43:23 +0530

    user authentication

--- nodejs-typescript/src/index.ts
+++ nodejs-typescript/src/index.ts
@@ -1,39 +1,50 @@
-import express from "express";
+import express, {response} from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import userModel from "./models/user.model";
 import UserModel from "./models/user.model";
+import CustomeResponse from "./dtos/custome.response";
 //invoking the express
-const app =express();
+const app = express();
 
 //applying middleware
-//invoking bodyparser
+//invoking body-parser
 app.use(bodyParser.json())
-interface user{
-    _id:string
-    fName:string
-    lName:string
-    email:string
-    password:string
+
+interface user {
+    _id: string
+    fName: string
+    lName: string
+    email: string
+    password: string
 }
 
-let user:user[]=[];
+let user: user[] = [];
 
 //in node js this is not the endpoint this called as the routes
 //Because routes are coming with the express
-app.get('/user/all', (req:express.Request , res:express.Response) => {
+app.get('/user/all', async (req: express.Request, res: express.Response) => {
 
-    // let data ={
-    //     _id:"M-001",
-    //     fName:"Dilshan",
-    //     lName:"Shivantha",
-    //     email:"dilshan@3002.com"
-    // }
+    try {
+        // let data ={
+        //     _id:"M-001",
+        //     fName:"Dilshan",
+        //     lName:"Shivantha",
+        //     email:"dilshan@3002.com"
+        // }
 
 
-    //the response that we need to send back when request is coming
-    //res.send("Hello");
-    res.send(user);
+        //the response that we need to send back when request is coming
+        //res.send("Hello");
+
+        const users = await  userModel.find();
+
+        res.send(
+            new CustomeResponse(200,"suceess" , users)
+        );
+    }catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 
@@ -42,41 +53,79 @@ app.get('/user/all', (req:express.Request , res:express.Response) => {
 *
 *Crate new user
 */
-app.post('/user' ,async (req:express.Request , res: express.Response) =>{
+app.post('/user', async (req: express.Request, res: express.Response) => {
 
-    //if you want to install out 3rd party library because after 2020 we can not use this directly
-    //Because it needs to convert to the format for that we use body-parser lib
-    //console.log(req.body);
-    user.push(req.body) // get request body and set that values to the user array at line [17]
+    try {
+        //if you want to install out 3rd party library because after 2020 we can not use this directly
+        //Because it needs to convert to the format for that we use body-parser lib
+        //console.log(req.body);
+        //user.push(req.body) // get request body and set that values to the user array at line [17]
 
-    const userModel = new UserModel({
-        username:req.body.username,
-        fName:req.body.fName,
-        lName:req.body.lName,
-        email:req.body.email,
-        password:req.body.password
-    })
+        const userModel = new UserModel({
+            username: req.body.username,
+            fName: req.body.fName,
+            lName: req.body.lName,
+            email: req.body.email,
+            password: req.body.password
+        })
 
-   await userModel.save();
-    //set the response with the status code
-    res.status(201).send("user created success")
+        const user=await userModel.save();
+        user.password = "" /* set password to empty string to send response */
+        //set the response with the status code
+        res.status(201).send("user created success")
+    } catch (error) {
+        res.status(500).send("can not save the user");
+    }
+
 })
+
+
+app.post("/user/auth",async (req: express.Request, res: express.Response) => {
+
+    try{
+        const user = await UserModel.findOne({email:req.body.email})
+        console.log("a")
+        if (user){
+            console.log("a")
+            if (user.password === req.body.password){
+                res.send(new CustomeResponse(200,"access").toJson());
+                // new CustomeResponse(200,"access")
+            }else {
+                res.send(new CustomeResponse(200,"wrong credentials").toJson())
+                new CustomeResponse(200,"wrong credentials")
+            }
+        }else{
+            res.send(new CustomeResponse(200,"user not found"))
+            // new CustomeResponse(200,"user not found")
+        }
+
+    }catch (error){
+        res.status(500).send(error)
+    }
+
+});
+
+
+/* article */
+
+
+
 
 // create connection with mongodb using mongoose
 mongoose.connect("mongodb://localhost/blog");
 const db = mongoose.connection;
 
 //show error if not connected to the mongodb
-db.on('error', (error) =>{
+db.on('error', (error) => {
     console.log(error);
 })
 
 //show success if connected to the mongodb
-db.on('open', () =>{
+db.on('open', () => {
     console.log("DB connected success");
 })
 
 // assign server port and tell the express to listen that port
-app.listen(8080 , ()=>{
+app.listen(8080, () => {
     console.log("server is started");
 })
